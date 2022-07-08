bbfa9250aa79ba12fc6d9d52d1e0cb6ad2b4b4ab
Shlomo isaacs <59068386+panguin6010@users.noreply.github.com>
2022-07-07 20:13:13 -0700

    add joke
    tehe beep boop

--- jokes.py
+++ jokes.py
@@ -1,21 +1,34 @@
 #Written by nxt & panguin6010
 
 print('Want to hear a joke?')
-userchoice = input()
+userchoice = raw_input()
 
 if userchoice == 'yes':
     print('Did you hear about the first restaurant to open on the moon?')
-    input()
+    raw_input()
     print('it had great food, but no atmosphere')
     print('Want to hear another joke?')
-    userchoice2 = input()
+    userchoice2 = raw_input()
 
     if userchoice2 == 'yes':
         print('Do you want to hear a construction joke')
-        input()
+        raw_input()
         print('im still working on it')
         print('haha,haha,haha')
-        quit()
+        print('Want to hear another joke?')
+        userchoice2 = raw_input()
+
+        if userchoice2 == 'yes':
+            print('Why dont eggs tell jokes?')
+            raw_input()
+            print('Theyd crack each other up.')
+            print('tehe beepboop I have no emotions ;(')
+            quit()
+        
+        else:
+            print(';(')
+            quit()
+
 
     else:
         print(';(')
